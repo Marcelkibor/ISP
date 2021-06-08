23c9a2679ea03b39b1df18b59f5801d10c2feac6
Marcel <kibormarcel90@gmail.com>
2021-06-08 11:42:46 +0300

    nav bar fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react'
-import { DesktopOutlined,PieChartOutlined,FileOutlined,TeamOutlined,UserOutlined} from '@ant-design/icons';
+import { DesktopOutlined,PieChartOutlined,FileOutlined,TeamOutlined,UserOutlined, HomeOutlined} from '@ant-design/icons';
 import{Menu, Breadcrumb,Layout} from 'antd'
 import './header.css'
 import 'antd/dist/antd.css'
+import {Link} from 'react-router-dom'
 const { Header, Content, Footer, Sider } = Layout;
 const { SubMenu } = Menu;
 
@@ -19,40 +20,50 @@ class Heading extends React.Component {
   
     render() {
       const { collapsed } = this.state;
+      const onclick =()=>{
+        console.log('you can now apply redirection')
+      }
       return (
-        <Layout style={{ minHeight: '100vh' }}>
+       
+        <Layout style={{ minHeight: '100vh',marginTop:'-50px' }}>
           <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
             <div className="logo" />
-            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-              <Menu.Item key="1" icon={<PieChartOutlined />}>
-                Option 1
+            <Menu theme="dark" defaultSelectedKeys={['1']} >
+              <Menu.Item as ={Link} name ='home'to ='home' onClick ={onclick}icon={<HomeOutlined />}>
+                Home
               </Menu.Item>
-              <Menu.Item key="2" icon={<DesktopOutlined />}>
-                Option 2
+              <Menu.Item as ={Link} name ='login'to ='login' onClick ={onclick} icon={<UserOutlined/>}>
+                Login
               </Menu.Item>
-              <SubMenu key="sub1" icon={<UserOutlined />} title="User">
+              {/* <SubMenu key="sub1" icon={<UserOutlined />} title="User">
                 <Menu.Item key="3">Tom</Menu.Item>
                 <Menu.Item key="4">Bill</Menu.Item>
                 <Menu.Item key="5">Alex</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
+              </SubMenu> */}
+              
+              
+              <Menu.Item as ={Link} name ='register'to ='register'onClick ={onclick}icon={<TeamOutlined/>}>Register</Menu.Item>
+              {/* <SubMenu key="sub2" icon={<TeamOutlined />} title="Team">
                 <Menu.Item key="6">Team 1</Menu.Item>
                 <Menu.Item key="8">Team 2</Menu.Item>
               </SubMenu>
-              <Menu.Item key="9" icon={<FileOutlined />}>
-                Files
-              </Menu.Item>
+               */}
             </Menu>
           </Sider>
           <Layout className="site-layout">
-            <Header className="site-layout-background" style={{ padding: 0 }} />
-            <Content style={{ margin: '0 16px' }}>
-              <Breadcrumb style={{ margin: '16px 0' }}>
-                <Breadcrumb.Item>User</Breadcrumb.Item>
-                <Breadcrumb.Item>Bill</Breadcrumb.Item>
+            <Header className="site-layout-background" style={{ padding: 0, marginTop:'-20px', }} />
+            <Content style={{ margin: '0px 20px ' }}>
+              <Breadcrumb style={{ margin: '10px' }}>
+                <Breadcrumb.Item><h2 style={{color:'darkblue',textAlign:'center' ,fontWeight:'bolder'}}>EVERYDAY</h2></Breadcrumb.Item>
+               <Breadcrumb.Item>Quick Action</Breadcrumb.Item>
+               <Breadcrumb.Item>Quick Action</Breadcrumb.Item>
+               <Breadcrumb.Item>Quick Action</Breadcrumb.Item>
               </Breadcrumb>
-              <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-                Bill is a cat.
+              <div className="site-layout-background" style={{ padding: 24, minHeight: 600 }}>
+                <h3>
+                  Everyday
+                </h3>
+                <p>######### <br></br>  Add home content here <br></br> #####################</p>
               </div>
             </Content>
             <Footer style={{ textAlign: 'center' }}>ISP ©2021 Created by Marcel & Silas</Footer>
